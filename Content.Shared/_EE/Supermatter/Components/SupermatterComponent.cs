8714049ba8003a0aabdfe722bf1cf93e5850239a
Fat Engineer <fatengineergaming2007@gmail.com>
2025-10-09 19:58:53 -0400

    SM KUDZU TODAY

--- Content.Shared/_EE/Supermatter/Components/SupermatterComponent.cs
+++ Content.Shared/_EE/Supermatter/Components/SupermatterComponent.cs
@@ -57,9 +57,9 @@ public sealed partial class SupermatterComponent : Component
     [DataField]
     public EntProtoId TeslaSpawnPrototype = "TeslaEnergyBall";
 
-    // one day...
-    // [DataField]
-    // public EntProtoId KudzuSpawnPrototype = "SupermatterKudzu";
+    // TODAY
+    [DataField]
+    public EntProtoId KudzuSpawnPrototype = "SupermatterKudzu";
 
     [DataField]
     public EntProtoId AnomalyBluespaceSpawnPrototype = "AnomalyBluespace";
